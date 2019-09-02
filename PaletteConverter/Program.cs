4658c30ab9939574808c4caef657727bf925ea27
Jack Vine <jackv949@gmail.com>
2019-09-02 21:15:27 +0930

    Basic working

---
+++ PaletteConverter/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+
+namespace PaletteConverter
+{
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+			string filePath = args[0];
+
+			var colors = new List<Color>();
+
+			var attr = File.GetAttributes(filePath);
+			if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+			{
+				Console.WriteLine("Processing directory..");
+
+				string[] files = Directory.GetFiles(filePath, "*.png", SearchOption.AllDirectories);
+				foreach (var file in files)
+				{
+					string newPath = GetFilePathRelative(filePath, file, "Processed");
+
+					if (!ProcessImage(file, newPath, colors))
+						return;
+				}
+			}
+			else
+			{
+				Console.WriteLine("Processing individual file..");
+
+				string newPath = filePath.Substring(0, filePath.Length - 4) + "_processed.png";
+
+				if (!ProcessImage(filePath, newPath, colors))
+					return;
+			}
+
+			var lut = new Bitmap(256, 1);
+			for (int i = 0; i < 256; i++)
+			{
+				lut.SetPixel(i, 0, i < colors.Count ? colors[i] : Color.Black);
+			}
+
+			string lutPath = "LUT.png";
+
+			lut.Save(lutPath, ImageFormat.Png);
+			lut.Dispose();
+			Console.WriteLine($"Saved {lutPath}");
+		}
+
+		private static bool ProcessImage(string filePath, string newPath, List<Color> colors)
+		{
+			Console.WriteLine($"Processing file: {filePath}");
+
+			var bmp = new Bitmap(filePath);
+
+			for (int y = 0; y < bmp.Height; y++)
+			{
+				for (int x = 0; x < bmp.Width; x++)
+				{
+					var pixel = bmp.GetPixel(x, y);
+					var lutPixel = Color.FromArgb(pixel.R, pixel.G, pixel.B);
+
+					int index = colors.IndexOf(lutPixel);
+					if (index < 0)
+					{
+						index = colors.Count;
+						colors.Add(lutPixel);
+					}
+
+					if (colors.Count >= 255)
+					{
+						Console.WriteLine("ERROR: More than 256 colors found!");
+						return false;
+					}
+
+					var newPixel = Color.FromArgb(pixel.A, index, 0, 0);
+					bmp.SetPixel(x, y, newPixel);
+				}
+			}
+
+			bmp.Save(newPath, ImageFormat.Png);
+			bmp.Dispose();
+			Console.WriteLine($"Saved {newPath}");
+
+			return true;
+		}
+
+		private static string GetFilePathRelative(string sourceRootPath, string sourcePath, string targetPath)
+		{
+			if (string.IsNullOrWhiteSpace(sourcePath))
+				throw new ArgumentNullException("sourcePath");
+
+			if (string.IsNullOrWhiteSpace(targetPath))
+				throw new ArgumentNullException("destinationPath");
+
+			if (!File.Exists(sourcePath))
+				throw new FileNotFoundException(sourcePath);
+
+			// trimmedPath becomes the file path with all the subfolders, but without the
+			// sourceRootPath that comes in front of it. i.e. it strips the value passed
+			// in sourceRootPath from the value passed in sourcePath. The "+ 1" is to include the
+			// trailing "\" in the path.
+			string trimmedPath = sourcePath.Substring(sourceRootPath.Length + 1);
+			string newPath = Path.Combine(targetPath, trimmedPath);
+			string fileName = Path.GetFileName(sourcePath);
+
+			// folderStructure is used for creating the subfolder structure I want to preserve.
+			// (It is just removing the file name and extension from the newPath.)
+			string folderStructure = newPath.Substring(0, (newPath.Length - fileName.Length));
+
+			// Directory.CreateDirectory will create the entire folder structure for me; no need
+			// for looping or recursive calls.
+			Directory.CreateDirectory(folderStructure);
+
+			return newPath;
+		}
+	}
+}
